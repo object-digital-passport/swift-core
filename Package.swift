b81fcd07b5706f899a3acd129318c99d6182085d
Andrei Chernikov <andrei.chernikov@proton.me>
2026-08-24 14:37:05 +0300

    build: support Swift 6.0 and iOS 18

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "swift-core",
-    platforms: [.iOS(.v26), .macOS(.v15)],
+    platforms: [.iOS(.v18), .macOS(.v15)],
     products: [
         // Everything a conforming implementation needs, with no platform
         // dependencies: canonical form, hashes, registry reads, wallet crypto.
